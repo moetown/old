b26241a30cf8601087c3c42848942567b2983984
moetown <moaparsland@gmail.com>
2017-06-22 10:10:54 +1000

    testing stuff

--- js/custom.js
+++ js/custom.js
@@ -177,5 +177,26 @@ $(document).ready(function(){
 
   ga('create', 'UA-101421410-1', 'auto');
   ga('send', 'pageview');
+  
+  
+  /* slideshow
+-----------------------------------------------*/
+  var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
 
 
